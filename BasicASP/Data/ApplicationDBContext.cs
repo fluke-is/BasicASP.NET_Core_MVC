288289c978cef4457fc0786019c5f98e9fa12ca1
fluke-is <fluke2000tv@gmail.com>
2023-06-06 18:29:53 +0700

    Add project files.

---
+++ BasicASP/Data/ApplicationDBContext.cs
@@ -0,0 +1,15 @@
+﻿using BasicASP.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BasicASP.Data
+{
+    public class ApplicationDBContext:DbContext
+    {
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options) :base(options) {
+            
+        }
+
+        public DbSet<Student> Students { get; set; }
+
+    }
+}
